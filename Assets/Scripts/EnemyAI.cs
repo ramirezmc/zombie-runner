019da875d026b1d6375f728b4379906c0a6d8986
ramirezmc <ramirezmiguelcarlo@gmail.com>
2022-11-15 17:43:59 +0800

    Start of EnemyAI Functionality
    EnemyAI has now chase function and chase range to the player and is visible on the scene view with OnDrawGizmoSelected

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+	[SerializeField]Transform target;
+	[SerializeField]float chaseRange = 5f;
+	
+	NavMeshAgent navMeshAgent;
+	float distanceToTarget = Mathf.Infinity;
+	
+    void Start()
+    {
+	    navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+	{
+		EnemyChase();
+    }
+    
+	void EnemyChase()
+	{
+		//Follows the player if within range
+		distanceToTarget = Vector3.Distance(target.position, transform.position);
+		if (distanceToTarget <= chaseRange)
+		{
+			navMeshAgent.SetDestination(target.position);
+		}
+	}
+	
+	void OnDrawGizmosSelected()
+	{
+		//Shows chase range of enemy
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, chaseRange);
+	}
+}
